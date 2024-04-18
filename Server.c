41feea02322055496501b8e502fcfad144f94d0a
kingsman-svg <3457169150@qq.com>
2024-04-18 23:23:58 +0800

    update

---
+++ Server.c
@@ -0,0 +1,293 @@
+#include"Server.h"
+#include<arpa/inet.h> //声明了更多的套接字相关的函数
+#include<sys/epoll.h>
+#include<stdio.h>
+#include<fcntl.h>
+#include<errno.h>
+#include<strings.h>
+#include<string.h>
+#include<sys/stat.h>
+#include<assert.h>
+#include<sys/sendfile.h>
+#include<dirent.h>
+#include<unistd.h>
+#include<stdlib.h>
+
+
+int initListenFd(unsigned short port) {
+
+	//1.创建监听的fd
+	int lfd = socket(AF_INET, SOCK_STREAM, 0);//ipv4，流式传输
+	if (lfd == -1) {
+		//套接字创建失败
+		perror("socket");
+		return -1;
+	}
+
+	//2.设置端口复用
+	int opt = 1;//1表示设置lfd可以复用绑定的端口 (2msl期间使用绑定的端口)
+	int ret = setsockopt(lfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+	if (ret == -1) {
+		perror("setsockopt");
+		return -1;
+	}
+	//3.绑定
+	struct sockaddr_in addr;
+	addr.sin_family = AF_INET;//指定ipv4协议
+	addr.sin_port = htons(port);//指定端口
+	addr.sin_addr.s_addr = INADDR_ANY;//绑定本地计算机随机一个网卡
+	ret = bind(lfd, (struct socketaddr*)&addr, sizeof(addr));
+	if (ret == -1) {
+		perror("bind");
+		return -1;
+	}
+	//4.设置监听
+	ret = listen(lfd, 128);
+	if (ret == -1) {
+		perror("listen");
+		return -1;
+	}
+	//5.返回fd
+	return lfd;
+}
+
+int epollRun(int lfd) {
+	//1.创建epoll实例
+	int epfd = epoll_create(1);
+	if (epfd == -1) {
+		perror("epoll_create");
+		return -1;
+	}
+	//2.添加lfd
+	struct epoll_event ev;
+	ev.data.fd = lfd;
+	ev.events = EPOLLIN;
+	int ret = epoll_ctl(epfd, EPOLL_CTL_ADD, lfd, &ev);
+	if (ret == -1) {
+		perror("epoll_ctl");
+		return -1;
+	}
+	//3.检测
+	struct epoll_event evs[1024];
+	int size = sizeof(evs) / sizeof(struct epoll_event);
+	while (1)
+	{
+		int num = epoll_wait(epfd, evs, size, -1);//返回触发的事件的个数
+		for (int i = 0; i < num; i++) {
+			int fd = evs[i].data.fd;//取出fd
+			if (fd == lfd) {
+				//如果是用于监听的文件描述符
+				//建立新连接 accept
+				acceptClient(lfd, epfd);
+
+			}
+			else {
+				//如果是用于通信的文件描述符
+				//主要是接受客户端的数据
+				recvHttpRequest(fd, epfd);
+			}
+		}
+	}
+}
+
+int acceptClient(int lfd, int epfd) {
+	//1、建立连接
+	int cfd = accept(lfd, NULL, NULL);
+	if (cfd == -1) {
+		perror("accept");
+		return -1;
+	}
+	//2、设置非阻塞
+	int flag = fcntl(cfd, F_GETFL);
+	flag |= O_NONBLOCK;
+	fcntl(cfd, F_SETFL,flag);
+	//3、cfd添加到epoll中
+	struct epoll_event ev;
+	ev.data.fd = cfd;
+	ev.events = EPOLLIN | EPOLLET;
+	int ret = epoll_ctl(epfd, EPOLL_CTL_ADD, cfd, &ev);
+	if (ret == -1) {
+		perror("epoll_ctl");
+		return -1;
+	}
+	return 0;
+}
+
+int recvHttpRequest(int cfd, int epfd) {
+	printf("开始接收数据了...");
+
+	int len = 0, totle = 0;
+	char tmp[1024] = { 0 };
+	char buf[4096] = { 0 };//存储数据的内存,主要是请求头
+	while ((len = recv(cfd, tmp, sizeof tmp, 0)) > 0) {
+		if (totle + len < sizeof(buf)) {
+			memcpy(buf + totle, tmp, len);
+		}
+		totle += len;
+	}
+	//判断数据是否被接收完毕
+	//recv在数据读完或者读取数据失败时都会返回-1
+	//可以通过errno的值来判断
+	if (len == -1 && errno == EAGAIN) {
+		//解析请求行并且发送数据(只考虑get请求)
+		char* pt = strstr(buf, "\r\n");
+		int reqLen = pt - buf;
+		buf[reqLen] = '\0';
+		parseRequestLine(buf, cfd);
+	}
+	else if (len == 0) {
+		//客户端已经断开连接
+		epoll_ctl(epfd, EPOLL_CTL_DEL, cfd, NULL);
+		close(cfd);
+	}
+	else {
+		perror("recv");
+	}
+	return 0;
+}
+
+int parseRequestLine(const char* line, int cfd) {
+	//解析请求行 
+	//get请求行的格式:get /xxx/1.jpg http/1.1
+	char method[12];
+	char path[1024];
+	sscanf(line, "%[^ ] %[^ ]", method, path);
+
+	printf("method: %s,path: %s\n", method, path);
+
+	if (strcasecmp(method, "get") != 0) {
+		return -1;
+	}
+	//处理客户端请求的静态资源(目录或者文件)
+	char* file = NULL;
+	if (strcmp(path, "/") == 0) {
+		file = "./";
+	}
+	else {
+		file = path + 1;
+	}
+	//获取文件属性
+	struct stat st;
+	int ret = stat(file, &st);
+	if (ret == -1) {
+		//文件不存在 -- 回复404
+		sendHeadMsg(cfd, 404, "Not Found", getFileType(".html"), -1);
+		sendFile("404.html", cfd);
+		return 0;
+	}
+	//文件存在，判断文件类型
+	if (S_ISDIR(st.st_mode)) {
+		//如果是目录
+		//把这个目录中的内容发送给客户端
+		sendHeadMsg(cfd, 200, "OK", getFileType(".html"), -1);
+		sendDir(file, cfd);
+	}
+	else {
+		//如果是文件
+		//把文件的内容发送给客户端
+		sendHeadMsg(cfd, 200, "OK", getFileType(file), st.st_size);
+		sendFile(file, cfd);
+	}
+
+	return 0;
+	
+
+}
+
+int sendFile(const char* fileName, int cfd) {
+	//1.打开文件
+	int fd = open(fileName, O_RDONLY);
+	assert(fd > 0);
+	//2.发送文件
+	int size = lseek(fd, 0, SEEK_END);
+	sendfile(cfd, fd, NULL, size);
+	return 0;
+}
+
+int sendHeadMsg(int cfd, int status, const char* descr, const char* type, int length) {
+	//状态行
+	char buf[4096] = { 0 };
+	sprintf(buf, "http/1.1 %d %s\r\n", status, descr);
+	//响应头
+	sprintf(buf, +strlen(buf), "content-type: %s\r\n", type);
+	sprintf(buf, +strlen(buf), "content-type: %d\r\n\r\n", length);
+	//发送数据块
+	send(cfd, buf, strlen(buf), 0);
+	return 0;
+}
+
+const char* getFileType(const char* name) {
+	const char* dot = strrchr(name, ".");
+	if (dot == NULL)
+		return "text/plain;charset=utf-8";
+	if (strcmp(dot, ".html") == 0 || strcmp(dot, ".htm" == 0))
+		return "text/html;charset=utf-8";
+	if (strcmp(dot, ".jpg") == 0 || strcmp(dot, ".jpeg" == 0))
+		return "image/jpeg";
+	if (strcmp(dot, ".gif") == 0 )
+		return "iamge/gif";
+	if (strcmp(dot, ".png") == 0)
+		return "iamge/png";
+	if (strcmp(dot, ".css") == 0)
+		return "text/css";
+	if (strcmp(dot, ".au") == 0)
+		return "audio/basic";
+	if (strcmp(dot, ".wav") == 0)
+		return "audio/wav";
+	if (strcmp(dot, ".avi") == 0)
+		return "video/x-msvideo";
+	if (strcmp(dot, ".mov") == 0 || strcmp(dot, ".qt") == 0)
+		return "video/quicktime";
+	if (strcmp(dot, ".mpeg") == 0 || strcmp(dot, ".mpe") == 0)
+		return "video/mpeg";
+	if (strcmp(dot, ".vrml") == 0 || strcmp(dot, ".wrl") == 0)
+		return "model/vrml";
+	if (strcmp(dot, ".midi") == 0 || strcmp(dot, ".mid") == 0)
+		return "audio/midi";
+	if (strcmp(dot, ".mp3") == 0)
+		return "audio/mpeg";
+	if (strcmp(dot, ".ogg") == 0)
+		return "application/ogg";
+	if (strcmp(dot, ".pac") == 0)
+		return "application/x-ns-proxy-autoconfig";
+
+	return "text/plain;charset=utf-8";
+}
+
+int sendDir(const char* dirName, int cfd)
+{
+	char buf[4096] = { 0 };
+	sprintf(buf, "<html><head><title>%s</title></head><body><table>", dirName);
+	struct dirent** namelist;
+	int num = scandir(dirName, &namelist, NULL, alphasort);
+	for (int i = 0; i < num; ++i) {
+		//取出文件名 
+		//namelist指向的是一个指针数组 struct dirent* tmp[]
+		char* name = namelist[i]->d_name;
+		struct stat st;
+		char subPath[1024] = { 0 };
+		sprintf(subPath, "%s/%s", dirName, name);
+		stat(subPath, &st);
+		if (S_ISDIR(st.st_mode))
+		{
+			//如果是目录
+			sprintf(buf + strlen(buf), 
+				"<tr><td><a href=\"%s/\">%s</a>%s</td><td>%ld</td></tr>", 
+				name, st.st_size);
+		}
+		else
+		{
+			//如果是文件
+			sprintf(buf + strlen(buf),
+				"<tr><td><a href=\"%s/\">%s</a></td><td>%ld</td></tr>",
+				name, st.st_size);
+		}
+		send(cfd, buf, strlen(buf), 0);
+		memset(buf, 0, sizeof(buf));
+		free(namelist[i]);
+	}
+	sprintf(buf, "</table></body></html>");
+	send(cfd, buf, sizeof(buf), 0);
+	free(namelist);
+	return 0;
+}
---
+++ main.c
@@ -0,0 +1,29 @@
+﻿#include"Server.h"
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+
+
+int main(int argc,char* argv[])
+{
+    if (argc < 3) {
+        printf("./a.out port path\n");
+        return -1;
+    }
+    unsigned short port = atoi(argv[1]);
+    //切换服务器的工作路径
+    chdir(argv[2]);
+    //初始化一个用于监听的套接字
+    int lfd = initListenFd(port);
+
+    //基于该套接字启动服务器程序
+    //使用epoll
+    epollRun(lfd);
+    return 0;
+    
+    //程序启动后，将基于该套接字，一直检测客户端是否有新连接
+    //如果有新连接，建立连接
+    //连接建立成功后，会得到一个用于通信的文件描述符
+    //使用该文件描述符，与客户端进行通信
+
+}
